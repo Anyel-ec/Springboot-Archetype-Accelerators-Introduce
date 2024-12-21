3a029b5cd55176c8d26c3987e89f0a3021343647
anyel-ec <paulangel826@gmail.com>
2024-12-21 18:04:23 -0500

    chore: start project in spring boot

---
+++ src/test/java/top/anyel/archetypespringboot/ArchetypeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.archetypespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchetypeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
